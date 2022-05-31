7a1ef781d3b23c76b32a3ddebb42c74663747981
TanhaR31 <tanurezza99@gmail.com>
2022-06-01 01:55:32 +0600

    Complete Mod 17

---
+++ Mod17.js
@@ -0,0 +1,15 @@
+var vowels = ['a','e','i','o','u'];
+// console.log(vowels);
+// console.log(vowels.length);
+// console.log(vowels.indexOf('a'));
+// console.log(vowels.indexOf('u'));
+// console.log(vowels.indexOf('b'));
+// console.log(vowels[2]);
+var alph = ['a','e','i','o','u'];
+alph.push('b');
+// console.log(alph);
+alph.pop();
+alph.pop();
+// console.log(alph);
+// alph.shift();
+// console.log(alph);
---
+++ Mod19.js
@@ -0,0 +1,11 @@
+const name = 'Tanha';
+console.log(name);
+// name='Reja';
+// console.log(name);
+
+function inchToFeet(inch) {
+    return (inch / 12);
+}
+
+let myInch = 21;
+console.log(inchToFeet(myInch));
